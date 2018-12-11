ab7610b03b9d5b8f18c5681f28b02954e8e92f5f
diegoorellanaga <diegoorellanaga@gmail.com>
2018-12-10 20:02:47 -0500

    add plot to crime dash

--- boston-crime/crime_template.py
+++ boston-crime/crime_template.py
@@ -18,6 +18,7 @@ import pandas as pd
 import datetime as dt
 import random as rnd
 import numpy as np
+import plotly.graph_objs as go
 import itertools
 
 
@@ -32,6 +33,7 @@ coords = coords.T
 
 
 # Creating menus data
+#---------------------------------------------------------------------
 years = data.YEAR.unique()
 options_data_year = [{'label':str(year),'value':year} for year in years]
 
@@ -54,6 +56,12 @@ offenses = data.OFFENSE_DESCRIPTION.unique()
 options_data_offense = [{'label':offense,'value':offense} for offense in offenses]
 options_data_offense.append({'label': 'ALL', 'value': 'ALL'})
 
+columns_name = data.columns.values
+columns_name=np.append(columns_name,['ALL'])
+columns_name_options = [{'label':i,'value':i} for i in columns_name]
+
+
+#---------------------------------------------------------------------
 
 
 #Boston Coordinates
@@ -170,7 +178,38 @@ app.layout = html.Div([
         html.Div([html.Iframe(id = 'map', srcDoc = map_nyc.get_root().render(), width = '640', height = '480')],style = {'text-align': 'center'}),
         html.Hr(),        
 #----------------------------------------------------------------------------        
-        html.Div([html.H2('Data Plots')],style = {'text-align': 'center'}),         
+        html.Div([html.H2('Data Plots')],style = {'text-align': 'center'}),  
+        
+        html.Div([
+    
+        html.Div([html.Label('Select X Variable'),    dcc.Dropdown(
+        id='dropdown-X',
+        options=columns_name_options,
+        value='close'
+        )],style = {'width': '200','padding': 10, 'display': 'inline-block' }),
+
+#     html.Div([html.Label('Select Y Variable'),    dcc.Dropdown(
+#        id='dropdown-Y',
+#        options=columns_name_options,
+#        value='close'
+#    )]),
+    
+         html.Div([html.Label('Select filter_value Variable'),    dcc.Dropdown(
+            id='dropdown-filter_value',
+            options=columns_name_options,
+            value='close'
+        )],style = {'width': '200','padding': 10, 'display': 'inline-block' })],style = {'text-align': 'center'}),
+    
+    #    html.Div([html.Div(id='output-graph')]),
+        html.Div([ html.Button('plot data',id='button-3') ],style = {'text-align': 'center'}), 
+        html.Div([html.Hr()]),   
+        
+        html.Div([html.Div(id='output-graph-column')],style = {'text-align': 'center','padding': 20}),          
+        
+        
+        
+        
+        
         ])
 
 
@@ -272,7 +311,68 @@ def num_points_year(value_0,value_1,value_2,value_3,value_4,value_5):
 
     return "Total number of points: {0}. Number of unique locations: {1}".format(crime_by_year_month.shape[0],len(set(crime_by_year_month.Location.values))) 
     
+@app.callback(
+    Output('output-graph-column', 'children'),
+    [Input('button-3', 'n_clicks'),
+     Input('dropdown-X', 'value'),  
+     Input('dropdown-filter_value', 'value'),     
+     ])
+def plot_graph_column(n_clicks,x_value,filter_value):
+    global data
+    if n_clicks > 0:
+       
+        if filter_value != 'ALL':    
+            
+            return  dcc.Graph(
+                    id='life-exp-vs-gdp',
+                    figure={
+                        'data': [
+                            go.Bar(
+                                x=data.query("{1} == '{0}'".format(i,filter_value)).groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.index.get_values(),
+                                y=data.query("{1} == '{0}'".format(i,filter_value)).groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.values.reshape([data.query("{1} == '{0}'".format(i,filter_value)).groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.values.shape[0]]),
+                               # text=df[df['continent'] == i]['country'],
+                                marker={
+                                    'line': {'width': 0.5, 'color': 'white'}
+                                },
+                                name=str(i)
+                            ) for i in data['{0}'.format(filter_value)].unique()
+                        ],
+                        'layout': go.Layout(
+                            title ='Crimes by {0} filteres by {1}'.format(x_value,filter_value),  
+                            xaxis={'title': '{0}'.format(x_value)},
+                            yaxis={'title': 'Crimes'},
+                            margin={'l': 40, 'b': 40, 't': 40, 'r': 40},
+                            legend={'x': 0, 'y': 1},
+                            hovermode='closest'
+                        )
+                    }
+                )
+        else:
+            
+            return dcc.Graph(
+                    id='life-exp-vs-gdp',
+                    figure={
+                        'data': [
+                            go.Bar(
+                                x=data.groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.index.get_values(),
+                                y=data.groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.values.reshape([data.groupby(['{0}'.format(x_value)]).agg(['count']).OFFENSE_CODE.values.shape[0]]),
+                               # text=df[df['continent'] == i]['country'],
+                                marker={
+                                    'line': {'width': 0.5, 'color': 'white'}
+                                },
 
+                            ) 
+                        ],
+                        'layout': go.Layout(
+                            title ='Crimes by {0}'.format(x_value),    
+                            xaxis={'title': '{0}'.format(x_value)},
+                            yaxis={'title': 'Crimes'},
+                            margin={'l': 40, 'b': 40, 't': 40, 'r': 40},
+                            legend={'x': 0, 'y': 1},
+                            hovermode='closest'
+                        )
+                    }
+                )            
 
 
 
